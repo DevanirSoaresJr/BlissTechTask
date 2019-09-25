baf756c1342541e26d7b5d4f340e829be50f4359
Devanir Jr <devanirsoaresjr@gmail.com>
2019-09-25 09:40:08 +0100

    Updated choices EndPoint

--- app/src/main/java/devanir/soaresjunior/blisstechtaskdevanir/Constants.kt
+++ app/src/main/java/devanir/soaresjunior/blisstechtaskdevanir/Constants.kt
@@ -2,5 +2,6 @@ package devanir.soaresjunior.blisstechtaskdevanir
 
 const val BASE_URL = "https://private-anon-d8006b7753-blissrecruitmentapi.apiary-mock.com/"
 const val QUESTIONS_ENDPOINT = "questions?limit=limit&;offset=offse;filter=filter"
+const val CHOICES_ENDPOINT ="questions?question_filter=FILTER"
 const val NET_STATUS_ENDPOINT = "health"
 const val SHARE_ENDPOINT = "share?destination_email=destination_email&content_url=content_url"
--- app/src/main/java/devanir/soaresjunior/blisstechtaskdevanir/net/QuestionService.kt
+++ app/src/main/java/devanir/soaresjunior/blisstechtaskdevanir/net/QuestionService.kt
@@ -1,5 +1,6 @@
 package devanir.soaresjunior.blisstechtaskdevanir.net
 
+import devanir.soaresjunior.blisstechtaskdevanir.CHOICES_ENDPOINT
 import devanir.soaresjunior.blisstechtaskdevanir.NET_STATUS_ENDPOINT
 import devanir.soaresjunior.blisstechtaskdevanir.QUESTIONS_ENDPOINT
 import devanir.soaresjunior.blisstechtaskdevanir.data.Choice
@@ -7,13 +8,14 @@ import devanir.soaresjunior.blisstechtaskdevanir.data.QuestionsResponse
 import devanir.soaresjunior.blisstechtaskdevanir.data.StatusResponse
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface QuestionService {
 
     @GET(QUESTIONS_ENDPOINT)
     fun getQuestions(): Single<List<QuestionsResponse>>
 
-    @GET(QUESTIONS_ENDPOINT)
+    @GET(CHOICES_ENDPOINT)
     fun getChoices(): Single<List<Choice>>
 
     @GET(NET_STATUS_ENDPOINT)
